75d0e38617db94edb287156c8c1d51b5bb903f7d
Kasperkiewicz <144849163+Kasper74@users.noreply.github.com>
2023-11-17 15:15:53 +0100

    Generator.py

---
+++ Generator.py
@@ -0,0 +1,19 @@
+print("1 - Trójkąt Prostokątny")
+print("2 - Trójkąt Równoramienny")
+odpowiedz = int(input("Podaj opcję z powyższych: "))
+szerokosc = int(input("Podaj szerokość: "))
+if odpowiedz == 1:
+    if szerokosc % 2 == 0:
+        for i in range(1, szerokosc + 1, 2):
+            print(i * "*")
+
+    else:
+        print("Podaj parzystą liczbę!")
+
+if odpowiedz == 2:
+    if szerokosc % 2 == 0:
+        for i in range(1, szerokosc + 1, 2):
+            print((i * "*").center(szerokosc))
+
+    else:
+        print("Podaj parzystą liczbę!")
